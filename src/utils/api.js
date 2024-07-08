f7660472cd1b08464a5bded6623018716154f3aa
Siddharth Khatri <siddxharth@gmail.com>
2024-07-09 00:12:19 +0530

    Add gitignore and env for url endpoints

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_APP_BACKEND_URL;
+const apiPort = import.meta.env.VITE_APP_BACKEND_PORT;
+
 const api = axios.create({
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: `http://${"localhost"}:${"3001"}/api/v1`,
     headers: {
         "Content-Type": "application/json",
     },
